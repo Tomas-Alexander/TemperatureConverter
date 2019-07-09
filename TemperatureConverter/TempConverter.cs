cf626a10757503f9daf5b4b071b7cce6c40285ee
Tomas_Alexander <el.carta@hotmail.com>
2019-07-09 15:54:56 -0400

    correct project

---
+++ TemperatureConverter/TempConverter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+namespace TemperatureConverter
+{
+    public class TempConverter
+    {
+        public static string Convert(string path)
+        {
+            string text = File.ReadAllText(path);
+            string[] data = text.Split(",");
+            double result = Double.Parse(data[0]);
+            if (data[1] == "C")
+            {
+                if (data[2] == "F")
+                {
+                    result = ConvertFromCToF(double.Parse(data[0]));
+                }
+                else if (data[2] == "K")
+                {
+                    result = ConvertFromCToK(double.Parse(data[0]));
+                }
+
+            }
+            if (data[1] == "F")
+            {
+                if (data[2] == "C")
+                {
+                    result = ConvertFromFToC(double.Parse(data[0]));
+                }
+                else if (data[2] == "K")
+                {
+                    result = ConvertFromFToK(double.Parse(data[0]));
+                }
+
+            }
+
+            if (data[1] == "K")
+            {
+                if (data[2] == "C")
+                {
+                    result = ConvertFromKToC(double.Parse(data[0]));
+                }
+                if (data[2] == "F")
+                {
+                    result = ConvertFromKToF(double.Parse(data[0]));
+                }
+            }
+
+            result = Math.Round(result, 3);
+            return text + "," + result.ToString();
+        }
+
+        private static double ConvertFromKToF(double v)
+        {
+            return ConvertFromCToF(ConvertFromKToC(v));
+        }
+
+        private static double ConvertFromKToC(double value)
+        {
+            return value - 273.15;
+        }
+
+        private static double ConvertFromFToK(double value)
+        {
+            return ConvertFromFToC(value) + 273.15;
+        }
+
+        private static double ConvertFromCToK(double value)
+        {
+            return value + 273.15;
+        }
+
+        public static double ConvertFromCToF(double value)
+        {
+            return (value * (9.0 / 5.0)) + 32;
+        }
+
+        private static double ConvertFromFToC(double value)
+        {
+            return (value - 32) * (5.0 / 9.0);
+        }
+    }
+}
---
+++ TemperatureConverterTests/TemperatureConverterTest.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using TemperatureConverter;
+
+namespace TemperatureConverterTests
+{
+    [TestFixture]
+    public class TemperatureConverterTest
+    {
+        [Test]
+        public void ConvertingTemperature_With0DegreesFromCToF_ShouldReturn32F()
+        {
+            string path = @"C:\Users\elcar\OneDrive\Escritorio\TemperatureConverter Pruebas\Test1.txt";
+            string expected = "0,C,F,32";
+            string result = TempConverter.Convert(path);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ConvertingTemperature_With100DegreesFromCToF_ShouldReturn212F()
+        {
+            string path = @"C:\Users\elcar\OneDrive\Escritorio\TemperatureConverter Pruebas\Test2.txt";
+            string expected = "100,C,F,212";
+            string result = TempConverter.Convert(path);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ConvertingTemperature_With32DegreesFromFToC_ShouldReturn0C()
+        {
+            string path = @"C:\Users\elcar\OneDrive\Escritorio\TemperatureConverter Pruebas\Test3.txt";
+            string expected = "32,F,C,0";
+            string result = TempConverter.Convert(path);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ConvertingTemperature_With0DegreesFromCToK_ShouldReturn273K()
+        {
+            string path = @"C:\Users\elcar\OneDrive\Escritorio\TemperatureConverter Pruebas\Test4.txt";
+            string expected = "0,C,K,273.15";
+            string result = TempConverter.Convert(path);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ConvertingTemperature_With0DegreesFromFToK_ShouldReturn255K()
+        {
+            string path = @"C:\Users\elcar\OneDrive\Escritorio\TemperatureConverter Pruebas\Test5.txt";
+            string expected = "0,F,K,255.372";
+            string result = TempConverter.Convert(path);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ConvertingTemperature_With273DegreesFromKToC_ShouldReturn0C()
+        {
+            string path = @"C:\Users\elcar\OneDrive\Escritorio\TemperatureConverter Pruebas\Test6.txt";
+            string expected = "273.15,K,C,0";
+            string result = TempConverter.Convert(path);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ConvertingTemperature_With273DegreesFromKToF_ShouldReturn0C()
+        {
+            string path = @"C:\Users\elcar\OneDrive\Escritorio\TemperatureConverter Pruebas\Test7.txt";
+            string expected = "273.15,K,F,32";
+            string result = TempConverter.Convert(path);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
